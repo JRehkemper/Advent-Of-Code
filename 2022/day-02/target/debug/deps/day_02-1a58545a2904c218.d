622c9ee37fae2e8f3f8678ed42fd2f1e137eaf2e
JadBlackstone <jannik.rehkemper@gmail.com>
2023-10-10 17:34:43 +0200

    day-02

---
+++ 2022/day-02/target/debug/deps/day_02-1a58545a2904c218.d
@@ -0,0 +1,5 @@
+/home/jannik/git/Advent-Of-Code/2022/day-02/target/debug/deps/day_02-1a58545a2904c218.rmeta: src/main.rs
+
+/home/jannik/git/Advent-Of-Code/2022/day-02/target/debug/deps/day_02-1a58545a2904c218.d: src/main.rs
+
+src/main.rs:
---
+++ 2022/day-02/target/debug/deps/day_02-ab1065dc0619bb58.d
@@ -0,0 +1,5 @@
+/home/jannik/git/Advent-Of-Code/2022/day-02/target/debug/deps/day_02-ab1065dc0619bb58: src/main.rs
+
+/home/jannik/git/Advent-Of-Code/2022/day-02/target/debug/deps/day_02-ab1065dc0619bb58.d: src/main.rs
+
+src/main.rs:
