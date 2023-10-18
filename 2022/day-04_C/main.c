0c1cabd6da7c0a380b43b0fe1ea630eaf51c22b0
Rehkemper Jannik <n5557@cldv1071.nobilia.org>
2023-10-18 15:01:55 +0200

    day 4 in c part 1

--- 2022/day-04_C/main.c
+++ 2022/day-04_C/main.c
@@ -3,61 +3,50 @@
 #include <string.h>
 
 void processPair(char *line, int *elf1_start, int *elf1_end, int *elf2_start, int *elf2_end) {
-    printf("input: %s", line);
+    printf("\ninput: %s", line);
     char *elf1;
     char *elf2;
     elf1 = strsep(&line, ",");
     elf2 = line;
 
-
     char *end;
-    /* *elf1_start = strtol(elf1, &end, 10);
-    *elf1_end = strtol(&elf1[2], &end, 10);
-    *elf2_start = strtol(elf2, &end, 10);
-    *elf2_end = strtol(&elf2[2], &end, 10);*/
 
-    /* TODO: Fix pointers...*/
     char *part;
+    /* split string at - */
     part = strsep(&elf1, "-");
-    printf("part: %s\n", part);
-    *elf1_start = strtol(part, &end, 10);
-    printf("rest: %s\n", elf1);
-    elf1_end = elf1;
+
+    /* convert string to int */
+    *elf1_start = atoi(part);
+    *elf1_end = atoi(elf1);
 
     char *part2;
+    /* split string at - */
     part2 = strsep(&elf2, "-");
-    printf("part2: %s\n", part2);
+    /* convert string to int */
     *elf2_start = strtol(part2, &end, 10);
-    elf2_end = elf2;
-    printf("start: %d\n", *elf1_start);
-    printf("end: %d\n", elf1_end);
+    *elf2_end = atoi(elf2);
 }
 
 int compareRanges(int *elf1_start, int *elf1_end, int *elf2_start, int *elf2_end) {
-    // elf1 contains elf2
-    if (*elf1_start <= *elf2_start) {
-        if (*elf1_end >= *elf2_end) {
-            return 1;
-        }
-        else {
-            return 0;
-        }
-    }
-    // elf2 contains elf1
-    else if (*elf2_start <= *elf1_start) {
-        if (*elf2_end >= *elf1_end) {
-            return 1;
-        }
-        else {
-            return 0;
-        }
-    }
+    printf("elf1_start: %d \n", *elf1_start);
+    printf("elf1_end: %d \n", *elf1_end);
+    printf("elf2_start: %d \n", *elf2_start);
+    printf("elf2_end: %d \n", *elf2_end);
 
-    return 0;
+    // elf1 contains elf2
+    if (*elf1_start <= *elf2_start && *elf1_end >= *elf2_end) {
+        return 1;
+    }
+    else if (*elf2_start <= *elf1_start && *elf2_end >= *elf1_end) {
+        return 1;
+    }
+    else { 
+        return 0;
+    }
 }
 
 void readFile(int *duplicateCounter) {
-    FILE *file_ptr = fopen("example-input.txt", "r");
+    FILE *file_ptr = fopen("input.txt", "r");
 
     if (file_ptr == NULL) {
         printf("Unable to read file");
@@ -66,19 +55,25 @@ void readFile(int *duplicateCounter) {
     
     /* Buffer to read one line at a time */
     char buffer[255];
+    int lines_number = 0;
 
     /* read into bufffer, maximum of 255 Bytes from file_ptr*/
     while(fgets(buffer, 255, file_ptr) != NULL) {
+        lines_number += 1;
         int elf1_start;
         int elf2_start;
         int elf1_end;
         int elf2_end;
         processPair(buffer, &elf1_start, &elf1_end, &elf2_start, &elf2_end);
-        if (compareRanges(&elf1_start, &elf1_end, &elf2_start, &elf2_end) == 1) {
+        int answer = compareRanges(&elf1_start, &elf1_end, &elf2_start, &elf2_end);
+        if (answer == 1) {
             printf("true\n");
             *duplicateCounter += 1;
+            printf("%d\n", *duplicateCounter);
         }
     }
+
+    printf("lines: %d\n", lines_number);
 
     fclose(file_ptr);
 }
